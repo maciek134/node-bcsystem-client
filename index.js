b6c468e9e12bb942c012ca50e7c09423428404c7
maciek134 <maciek134@gmail.com>
2012-10-16 18:13:56 +0200

    Created TCP client.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-global.OPTIONS = require('./config.json');
-global.SDL = require('sdlmixer');
-global.TCP = require('net');
+var _startTime = Date.now();
+
+global.DEBUG = function (info) { if (OPTIONS.verbose) console.log('>> [' (Date.now() - _startTime) + ']: ' + info); };
+global.ERROR = function (error) { console.log('>>> ' + error.code + ':\n', error.stack); };
+
+global.Options = require('./config.json');
+global.Connection = require('./connection.js');
+global.Player = require('./player.js');
+
+Connection.start();
+Player.start();
+
+DEBUG('up and running.');
